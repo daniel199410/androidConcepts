b7369d0116b4a0ce5d6757983747de35d6f102e5
dcatanor <dasniel199410@gmail.com>
2021-04-14 11:49:04 -0500

    Building fragment_movie.xml

---
+++ MovieApp/app/src/main/java/com/example/movieapp/MovieDetailFragment.kt
@@ -0,0 +1,14 @@
+package com.example.movieapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.View
+
+/**
+ * A simple [Fragment] subclass as the second destination in the navigation.
+ */
+class MovieDetailFragment : Fragment(R.layout.fragment_movie_detail) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ MovieApp/app/src/main/java/com/example/movieapp/MovieFragment.kt
@@ -0,0 +1,14 @@
+package com.example.movieapp
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class MovieFragment : Fragment(R.layout.fragment_movie) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
