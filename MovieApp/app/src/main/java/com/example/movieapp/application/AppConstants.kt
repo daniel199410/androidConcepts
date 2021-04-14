b42b40427c028a92db6b8b4d1727c56f8b65fd8a
dcatanor <dasniel199410@gmail.com>
2021-04-14 16:49:41 -0500

    Creating retrofit

---
+++ MovieApp/app/src/main/java/com/example/movieapp/application/AppConstants.kt
@@ -0,0 +1,6 @@
+package com.example.movieapp.application
+
+object AppConstants {
+    const val BASE_URL= "https://api.themoviedb.org/3/"
+    const val API_KEY="b211d9b317e64cbf2aa860e62a994288"
+}
