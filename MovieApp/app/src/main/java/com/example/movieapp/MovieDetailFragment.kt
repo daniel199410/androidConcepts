9ab2bb9a43bd7a1d63c9d6db572bd92e90772eea
dcatanor <dasniel199410@gmail.com>
2021-04-14 14:41:42 -0500

    Adding view binding

--- MovieApp/app/src/main/java/com/example/movieapp/MovieDetailFragment.kt
+++ MovieApp/app/src/main/java/com/example/movieapp/MovieDetailFragment.kt
@@ -3,12 +3,16 @@ package com.example.movieapp
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
+import com.example.movieapp.databinding.FragmentMovieDetailBinding
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
 class MovieDetailFragment : Fragment(R.layout.fragment_movie_detail) {
+    private lateinit var binding: FragmentMovieDetailBinding
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = FragmentMovieDetailBinding.bind(view)
     }
 }
--- MovieApp/app/src/main/java/com/example/movieapp/MovieFragment.kt
+++ MovieApp/app/src/main/java/com/example/movieapp/MovieFragment.kt
@@ -3,12 +3,16 @@ package com.example.movieapp
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import com.example.movieapp.databinding.FragmentMovieBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class MovieFragment : Fragment(R.layout.fragment_movie) {
+    private lateinit var binding: FragmentMovieBinding
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = FragmentMovieBinding.bind(view)
     }
 }
