5c4fae14d485b8cd970fec3d248650986962ca8b
dcatanor <dasniel199410@gmail.com>
2021-04-14 16:27:54 -0500

    Creating templates for methods

--- MovieApp/app/src/main/java/com/example/movieapp/data/remote/MovieDataSource.kt
+++ MovieApp/app/src/main/java/com/example/movieapp/data/remote/MovieDataSource.kt
@@ -1,4 +1,17 @@
 package com.example.movieapp.data.remote
 
+import com.example.movieapp.data.model.MovieList
+
 class MovieDataSource {
+    fun getUpcomingMovies(): MovieList {
+        return MovieList()
+    }
+
+    fun getTopRatedMovies(): MovieList {
+        return MovieList()
+    }
+
+    fun getTopPopularMovies(): MovieList {
+        return MovieList()
+    }
 }
