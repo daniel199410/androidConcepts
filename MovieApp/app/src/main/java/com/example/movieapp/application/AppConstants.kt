7a1fc45362364b4e12080123327527fae54923c6
dcatanor <dasniel199410@gmail.com>
2021-04-15 12:26:35 -0500

    Building MovieAdapter.kt

--- MovieApp/app/src/main/java/com/example/movieapp/application/AppConstants.kt
+++ MovieApp/app/src/main/java/com/example/movieapp/application/AppConstants.kt
@@ -3,4 +3,5 @@ package com.example.movieapp.application
 object AppConstants {
     const val BASE_URL= "https://api.themoviedb.org/3/"
     const val API_KEY="b211d9b317e64cbf2aa860e62a994288"
+    const val IMAGE_URL="https://image.tmdb.org/t/p/w500/"
 }
