5ae8d01f56734d50bb34db5e7f1341e7d17e065d
dcatanor <dasniel199410@gmail.com>
2021-04-21 15:54:31 -0500

    Navigating to Password Fragment

--- FlypassMobile/app/src/main/java/co/com/flypass/flypassNative/ui/login/Document.kt
+++ FlypassMobile/app/src/main/java/co/com/flypass/flypassNative/ui/login/Document.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import co.com.flypass.flypassNative.R
 import co.com.flypass.flypassNative.databinding.DocumentBinding
 
@@ -18,5 +19,11 @@ class Document: Fragment(R.layout.document){
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = DocumentBinding.bind(view)
+        binding.btnNext.setOnClickListener{
+            if(!binding.txtDocument.text.isNullOrBlank()) {
+                val action = DocumentDirections.actionDocumentToPassword(binding.txtDocument.text.toString())
+                findNavController().navigate(action)
+            }
+        }
     }
 }
