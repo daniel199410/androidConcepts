9a2d8c1dabe314a217674a5588dd83041eed08f8
dcatanor <dasniel199410@gmail.com>
2021-04-21 14:49:46 -0500

    Adding bindings

--- FlypassMobile/app/src/main/java/co/com/flypass/flypassNative/ui/login/Document.kt
+++ FlypassMobile/app/src/main/java/co/com/flypass/flypassNative/ui/login/Document.kt
@@ -1,13 +1,22 @@
 package co.com.flypass.flypassNative.ui.login
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import co.com.flypass.flypassNative.R
+import co.com.flypass.flypassNative.databinding.DocumentBinding
 
 class Document: Fragment(R.layout.document){
+    private lateinit var binding: DocumentBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         (requireActivity() as AppCompatActivity).supportActionBar?.hide()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = DocumentBinding.bind(view)
     }
 }
